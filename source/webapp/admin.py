9830800a7578634d424043349a2f09074cb80695
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-07-05 01:06:23 +0300

    Создал модель, таблицу, сделал миграцию

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'category', 'remains', 'price']
+    list_display_links = ['name']
+    list_filter = ['category']
+    search_fields = ['category']
+    fields = ['name', 'description', 'category', 'remains', 'price']
+    readonly_fields = []
+
+
+admin.site.register(Product, ProductAdmin)
