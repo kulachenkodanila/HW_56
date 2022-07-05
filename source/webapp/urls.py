517df3432b053d31f0d5a0c5f6b7bde52afa1905
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-07-05 13:00:30 +0300

    создал html фаил для просмотра отдельного продукта и зарегистрировал url

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from webapp.views import index_view
+from webapp.views import index_view, product_view
 
 urlpatterns = [
     path('', index_view, name="index"),
+    path('product/<int:pk>/', product_view, name="product_view"),
 
 ]
