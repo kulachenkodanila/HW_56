6f99c5f5eb1fc5107e776447d805ea3876784f70
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-07-05 15:39:00 +0300

    Добавил метод для удаления продукта и html фаил

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from webapp.views import index_view, product_view, create_product
+from webapp.views import index_view, product_view, create_product, delete_product, update_product
 
 urlpatterns = [
     path('', index_view, name="index"),
     path('product/<int:pk>/', product_view, name="product_view"),
     path('product/add/', create_product, name="create_product"),
-
+    path('product/<int:pk>/delete', delete_product, name="delete_product"),
+    path('product/<int:pk>/update', update_product, name="update_product"),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -29,8 +29,40 @@ def create_product(request):
             category = form.cleaned_data.get("category")
             remains = form.cleaned_data.get("remains")
             price = form.cleaned_data.get("price")
-            new_product = Product.objects.create(name=name, description=description, category=category, remains=remains, price=price)
+            new_product = Product.objects.create(name=name, description=description, category=category, remains=remains,
+                                                 price=price)
             return redirect("product_view", pk=new_product.pk)
         return render(request, "create.html", {"form": form})
 
 
+def delete_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == "GET":
+        return render(request, "delete.html", {"product": product})
+    else:
+        product.delete()
+        return redirect("index")
+
+
+def update_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == "GET":
+        form = ProductForm(initial={
+            "name": product.name,
+            "description": product.description,
+            "category": product.category,
+            "remains": product.remains,
+            "price": product.price,
+        })
+        return render(request, "update.html", {"form": form})
+    else:
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            product.name = form.cleaned_data.get("name")
+            product.description = form.cleaned_data.get("description")
+            product.category = form.cleaned_data.get("category")
+            product.remains = form.cleaned_data.get("remains")
+            product.price = form.cleaned_data.get("price")
+            product.save()
+            return redirect("index")
+        return render(request, "update.html", {"form": form})
