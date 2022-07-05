e123a102d5ff51b61dc8b7343f725eb64a523c23
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-07-05 12:17:39 +0300

    в фаил view добавил метод index_view(главную страницу), создал ytml фаил с главной страницей, добавил зарегистрировал url.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from webapp.models import Product
+
+
+def index_view(request):
+    products = Product.objects.order_by("category")
+    context = {"products": products}
+    return render(request, "index.html", context)
