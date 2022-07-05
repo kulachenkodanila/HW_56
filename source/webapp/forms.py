60b21fed4d64cb7514e21d12ad8a5caa2eac8ee1
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-07-05 13:24:58 +0300

    Добавил метод создания нового продукта

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -4,9 +4,8 @@ CATEGORY_CHOICES = [('other', 'Разное'), ('Milk', 'Молоко'), ('Juce'
 
 
 class ProductForm(forms.Form):
-    name = forms.CharField(max_length=100, null=False, blank=False, verbose_name="Name")
-    description = forms.TextField(max_length=2000, verbose_name="Description")
-    category = forms.CharField(max_length=30, choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES[0][0],
-                               verbose_name="Category")
-    remains = forms.IntegerField(verbose_name="Remains")
-    price = forms.DecimalField(max_digits=7, decimal_places=2, verbose_name="Price")
+    name = forms.CharField(max_length=100, label="Name")
+    description = forms.CharField(max_length=2000, label="Description")
+    category = forms.ChoiceField(choices = CATEGORY_CHOICES)
+    remains = forms.IntegerField(label="Remains")
+    price = forms.DecimalField(max_digits=7, decimal_places=2, label="Price")
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+from webapp.forms import ProductForm
 from webapp.models import Product
 
 
@@ -14,3 +15,22 @@ def product_view(request, **kwargs):
     pk = kwargs.get("pk")
     product = get_object_or_404(Product, pk=pk)
     return render(request, "product_view.html", {"product": product})
+
+
+def create_product(request):
+    if request.method == "GET":
+        form = ProductForm()
+        return render(request, "create.html", {"form": form})
+    else:
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            name = form.cleaned_data.get("name")
+            description = form.cleaned_data.get("description")
+            category = form.cleaned_data.get("category")
+            remains = form.cleaned_data.get("remains")
+            price = form.cleaned_data.get("price")
+            new_product = Product.objects.create(name=name, description=description, category=category, remains=remains, price=price)
+            return redirect("product_view", pk=new_product.pk)
+        return render(request, "create.html", {"form": form})
+
+
