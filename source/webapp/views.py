5d7557317488a47cef52c588eb74cfb86d0b0a29
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-07-05 12:20:56 +0300

    в фаил view добавил метод product_view(просмотр отдельного продукта)

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from webapp.models import Product
@@ -8,3 +8,9 @@ def index_view(request):
     products = Product.objects.order_by("category")
     context = {"products": products}
     return render(request, "index.html", context)
+
+
+def product_view(request, **kwargs):
+    pk = kwargs.get("pk")
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "product_view.html", {"product": product})
